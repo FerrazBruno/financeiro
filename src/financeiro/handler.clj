e48a538a4c8232760aca7dcdfda6700360337ac7
Bruno Ferraz <brunohenriqueferraz94@gmail.com>
2023-01-18 20:58:59 -0300

    Comeco de projeto + testes funcionais com midje

---
+++ src/financeiro/handler.clj
@@ -0,0 +1,11 @@
+(ns financeiro.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes
+  (GET "/" [] "Ola mundo")
+  (route/not-found "Recurso nao encontrado"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
---
+++ test/financeiro/handler_test.clj
@@ -0,0 +1,18 @@
+(ns financeiro.handler-test
+  (:require [midje.sweet :refer :all]
+            [ring.mock.request :as mock]
+            [financeiro.handler :refer :all]))
+
+(facts "Da um 'Ola mundo' na rota raiz"
+       (let [response (app (mock/request :get "/"))]
+         (fact "o status da resposta eh 200"
+               (:status response) => 200)
+         (fact "o texto do corpo eh 'Ola mundo'"
+               (:body response) => "Ola mundo")))
+
+(facts "Rota invalida nao existe"
+       (let [response (app (mock/request :get "/invalid"))]
+         (fact "o codigo de erro eh 404"
+               (:status response) => 404)
+         (fact "o texto do corpor eh 'Recurso nao encontrado'"
+               (:body response) => "Recurso nao encontrado")))
